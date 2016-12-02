f0e2ef955d6a8837bd21dcd319f4f67a38bb9ac1
Alexey <l500etc@gmail.com>
2016-12-02 14:11:08 +0200

    add ukrainian translation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :news_index
+  before_action :set_locale
 
   def news_index
     @novelties = Novelty.order(created_at: :desc).all.shuffle
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
