a12f8cc2a313d7df265e89bf0884b34b90618b3a
RoRElessar <palantiri@ukr.net>
2016-12-06 10:32:08 +0200

    change validation for announcements

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -2,7 +2,7 @@ class Announcement < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
   validates :main_image, presence: true
-  validates :title, length: { in: 10..20 }
+  validates :title, length: { in: 10..60 }
   validates :body, length: { minimum: 150 }
 
   belongs_to :admin_user
