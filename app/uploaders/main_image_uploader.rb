65e212391ca909dba32d5776df4691d3a27ccf96
Alexey <l500etc@gmail.com>
2016-12-09 14:29:24 +0200

    add styles for category show page

--- app/uploaders/main_image_uploader.rb
+++ app/uploaders/main_image_uploader.rb
@@ -40,8 +40,12 @@ class MainImageUploader < CarrierWave::Uploader::Base
      process :resize_to_fit => [50, 50]
    end
 
-  version :big do
+  version :medium do
     process :resize_to_fit => [100, 100]
+  end
+
+  version :big do
+    process :resize_to_fit => [300, 300]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
