baa76aa7e0662d922c37268ad7d5aeb387bfe6ce
Bogdan Soroka <srk.bogdan@gmail.com>
2017-02-03 12:14:21 +0200

    gallery slider

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,15 @@
 class WelcomeController < ApplicationController
 
+  before_action :gallery
+
   def home
     @announcements = Announcement.order(visits: :desc).limit(4).all
+  end
+
+  private
+
+  def gallery
+    @gallery = Gallery.find(1)
   end
 
 end
