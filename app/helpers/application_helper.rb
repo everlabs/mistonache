63ca8854c09b9ae37690fbb7151b99685efc044f
Oleg Pasko <olegpasko@gmail.com>
2017-03-06 00:16:40 +0200

    title helper added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def title_helper
+    base_name = 'Misto-na-4e'
+    if content_for?(:title)
+      content_for(:title) + ' | ' + base_name
+    else
+      base_name
+    end
+  end
 end
