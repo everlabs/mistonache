4973390cf063af95105cb70f539b361659473c2b
Oleg Pasko <olegpasko@gmail.com>
2017-03-16 17:47:07 +0200

    storage file instead of fog

--- app/uploaders/event_poster_uploader.rb
+++ app/uploaders/event_poster_uploader.rb
@@ -2,11 +2,7 @@ class EventPosterUploader < CarrierWave::Uploader::Base
 
   include CarrierWave::MiniMagick
 
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  storage :file
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
