24aa0e2e8a15d96c0b723ca59d52e598865a43f2
DenysNorman <denys.norman.tr@gmail.com>
2017-03-28 10:44:28 +0300

    add sort of go news in show page

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -8,6 +8,7 @@ class AnnouncementsController < ApplicationController
   def show
     @announcement = Announcement.find(params[:id])
     @announcement.update_attribute(:visits, @announcement.visits + 1)
+    @go_announcements = Announcement.go_feed.paginate(page: params[:page], per_page: 3)
   end
 
   def go_announcements
