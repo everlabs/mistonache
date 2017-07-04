9fce47ddceca58cba3a00304d4372c435fb7f7cf
Alexey <l500etc@gmail.com>
2017-07-04 12:23:04 +0300

    add url validation to banner

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -6,8 +6,11 @@ class Banner < ApplicationRecord
       kinoshot: 4,
       right_second: 5
   }
+  VALID_LINK = /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix
 
   validates :image, presence: true
+  validates :url, presence: true,
+            format: { with: VALID_LINK }
 
   mount_uploader :image, BannerUploader
   scope :by_kinoshot, -> { where("position = ?", Banner::POSITION[:kinoshot]) }
